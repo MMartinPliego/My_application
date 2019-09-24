b910986c09b1ff78bbcf0f35b3a5ed2812babb23
Manuel <manuel_jorgo@hotmail.com>
2019-09-24 08:51:59 +0200

    Add folders

---
+++ My_application/Data/Local/Models/Subject.swift
@@ -0,0 +1,43 @@
+//
+//  Subject.swift
+//  My_application
+//
+//  Created by Dev2 on 20/09/2019.
+//  Copyright © 2019 Orum Games. All rights reserved.
+//
+
+import Foundation
+
+class Subject: CustomStringConvertible { //CustomStringConvertible para que sepa pintar la vista
+    
+    var name: String?
+    var teachers: [Teacher] = [] //los arrays siempre devolverlos como vacios y nunca como nil u opcionales
+    var students: [Student] = []
+    
+    //Variable autocompotada, cada vez que se llama a la variable hace el cálculo (no guarda datos en ella)
+    var numTeachers: Int {
+        return teachers.count
+    }
+    
+    var numStudens: Int {
+        return students.count
+    }
+    
+    var description: String {
+        return "Subject data: \(String(describing: name))"
+    }
+    
+    //variable autocalculada
+    //    var numStudens: Int {
+    // return students?.count ?? 0
+    
+    convenience init(name: String, teachers: [Teacher] = [], students: [Student] = []) {
+        self.init ()
+        
+        self.name = name
+        self.teachers = teachers
+        self.students = students
+    }
+    
+    
+}
