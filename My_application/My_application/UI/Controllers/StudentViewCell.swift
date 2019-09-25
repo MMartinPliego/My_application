f3ed1524a8adc08a700e9b39ebd002a595c2903c
Manuel <manuel_jorgo@hotmail.com>
2019-09-25 19:43:55 +0200

    Create StudentViewCell

---
+++ My_application/My_application/UI/Controllers/StudentViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  StudentViewCell.swift
+//  My_application
+//
+//  Created by Dev2 on 25/09/2019.
+//  Copyright © 2019 Orum Games. All rights reserved.
+//
+
+import UIKit
+
+class StudentsViewCell: UITableViewCell {
+    
+        // MARK: - IBOutlets
+    @IBOutlet weak var studentView: UIView!
+    @IBOutlet weak var imageViewStudent: UIImageView!
+    @IBOutlet weak var labelViewStudenttittle: UILabel!
+    @IBOutlet weak var labelViewStudentdescription: UILabel!
+        
+        
+    
+}
