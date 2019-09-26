33c786f1ddb4593d120c119d660a699fe6b84044
Manuel <manuel_jorgo@hotmail.com>
2019-09-26 20:19:48 +0200

    Change Subject ViewController

--- My_application/My_application/UI/Controllers/SubjectsViewController.swift
+++ My_application/My_application/UI/Controllers/SubjectsViewController.swift
@@ -21,6 +21,15 @@ class SubjectsViewController: UIViewController {
         
         configureCollectionView()
         
+        // Para poner dos columnas
+        if let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout{
+                layout.minimumLineSpacing = 10
+                layout.minimumInteritemSpacing = 10
+                layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+                let size = CGSize(width:(collectionView!.bounds.width-30)/2, height: 250)
+                layout.itemSize = size
+        }
+        
     }
     
 }
